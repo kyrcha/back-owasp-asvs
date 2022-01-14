ffe58eacfefd2aba74702349fdea17e3190c01a4
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2022-01-14 18:49:03 +0200

    Add CORS

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
